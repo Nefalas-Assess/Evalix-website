3798d4ebdb751b8635a6db3d65a3d2892c10d7e1
Thomas Point <thomas@makeit-studio.com>
2025-11-09 13:49:04 +0100

    fix redirection home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -185,11 +185,11 @@ const Home = () => {
                   </p>
                 </div>
                 <div className="flex flex-col sm:flex-row gap-4 justify-center">
-                  <Button size="lg" className="bg-primary hover:bg-primary/90" onClick={() => window.location.href = t("home.test_license.cta_request")}>
+                  <Button size="lg" className="bg-primary hover:bg-primary/90" onClick={() => window.location.href = '/contacts'}>
                     {t("home.test_license.cta_request")}
                     <ArrowRight className="ml-2 h-5 w-5" />
                   </Button>
-                  <Button variant="outline" size="lg" onClick={() => window.location.href = t("home.test_license.cta_learn_more")}>
+                  <Button variant="outline" size="lg" onClick={() => window.location.href = '/presentation'}>
                     {t("home.test_license.cta_learn_more")}
                   </Button>
                 </div>
